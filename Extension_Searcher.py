c9ee2f4dfcf4b1e14535d9b13232a5c018bd8b9e
Jerry Guerra <jerry.gdr13@gmail.com>
2020-09-23 21:35:27 -0600

    Add files via upload

---
+++ Extension_Searcher.py
@@ -0,0 +1,13 @@
+import os
+
+path = '/Users/jguerrad/Downloads/'
+
+l = os.listdir(path)
+for e in l:
+    split_ext = e.split('.')[1]
+    filename = e
+    print(f"File name {e} and the extension is {split_ext}")
+
+def check_ext(a,b):
+    if i in range():
+        pass
